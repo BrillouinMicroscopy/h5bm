1304332fbe2dca148fe914534e85672255c5f44d
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2017-11-20 12:51:58 +0100

    Create h5 file on class init

--- h5bm.cpp
+++ h5bm.cpp
@@ -1,11 +1,36 @@
 #include "stdafx.h"
 #include "h5bm.h"
+#include "filesystem"
 
-H5BM::H5BM(QObject *parent)
+using namespace std::experimental::filesystem::v1;
+
+H5BM::H5BM(QObject *parent, const std::string filename, int flags)
 	: QObject(parent) {
-
-	file = H5Fcreate("test.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+	if (flags == H5F_ACC_RDONLY) {
+		if (exists(filename)) {
+			file = H5Fopen(&filename[0], flags, H5P_DEFAULT);
+		}
+	} else if (flags == H5F_ACC_RDWR) {
+		if (!exists(filename)) {
+			// create the file
+			file = H5Fcreate(&filename[0], H5F_ACC_EXCL, H5P_DEFAULT, H5P_DEFAULT);
+			// set the version attribute
+			hid_t type_id = H5Tcopy(H5T_C_S1);
+			H5Tset_size(type_id, versionstring.length());
+			hsize_t dims[1] = { 1 };
+			hsize_t maxdims[1] = { 1 };
+			hid_t space_id = H5Screate_simple(1, dims, maxdims);
+			hid_t attr_id = H5Acreate2(file, "version", type_id, space_id, H5P_DEFAULT, H5P_DEFAULT);
+			H5Awrite(attr_id, type_id, versionstring.c_str());
+			H5Aclose(attr_id);
+			H5Sclose(space_id);
+			H5Tclose(type_id);
+		} else {
+			file = H5Fopen(&filename[0], flags, H5P_DEFAULT);
+		}
+	}
 }
 
 H5BM::~H5BM() {
+	H5Fclose(file);
 }
--- h5bm.h
+++ h5bm.h
@@ -1,15 +1,22 @@
 #ifndef H5BM_H
 #define H5BM_H
 
+#include <string>
+
 #include "hdf5.h"
 
 class H5BM : public QObject {
 	Q_OBJECT
 
 public:
-	H5BM(QObject *parent = 0);
+	H5BM(
+		QObject *parent = 0,
+		const std::string filename = "Brillouin.h5",
+		int flags = H5F_ACC_RDONLY
+	);
 	~H5BM();
-	hid_t file;
+	const std::string versionstring = "H5BM-v0.0.3";
+	hid_t file;		// handle to the opened file
 
 };
 
